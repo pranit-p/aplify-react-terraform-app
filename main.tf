e72e9780d15559ad116148184d9d7b725564ba8c
pranit <pranitpawar880@gmail.com>
2021-05-04 17:11:50 +0530

    reformate files

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
   profile = "for-terraform-aws-access"
 }
 
 resource "aws_cloudformation_stack" "amplify-app" {
   name = "amplify-demo-app"
   parameters = {
-    Appname = var.app_name
-    RepositoryPath = var.repository_path
-    GitHubOuthTocken = var.GithubOauthToken
+    Appname           = var.app_name
+    RepositoryPath    = var.repository_path
+    GitHubOuthTocken  = var.GithubOauthToken
     DefaultBranchName = var.defauktbranchname
-    EnvironmentName = var.environment_name
+    EnvironmentName   = var.environment_name
   }
   template_body = file("${path.module}/template.yml")
 }
--- var.tf
+++ var.tf
@@ -1,22 +1,22 @@
 variable "environment_name" {
   description = "gives the name of environment like test,dev or something like that"
-  type= string
-  default = "dev"
+  type        = string
+  default     = "dev"
 }
 
 variable "app_name" {
-  default = "demo-app-name"
+  default     = "demo-app-name"
   description = "name of your amplify app"
-  type = string
+  type        = string
 }
 variable "repository_path" {
-  default = "https://github.com/pranit-p/demo-react-app"
+  default     = "https://github.com/pranit-p/demo-react-app"
   description = "path of your githube repository or it be any repository"
-  type = string
+  type        = string
 }
 
 variable "defauktbranchname" {
   description = "your default branch name on github"
-  type = string
-  default = "master"
+  type        = string
+  default     = "master"
 }
